e340a1b655ecbdcd21e7eda979a7dddc0a4a3a46
jinghaihan <jhh19980114@gmail.com>
2026-09-05 00:17:01 +0800

    test: include snapshot plugin and original build options

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'tsdown'
+import ApiSnapshot from 'tsnapi/rolldown'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
+  exports: true,
   dts: {
     tsgo: {},
   },
+  deps: {
+    onlyBundle: false,
+  },
+  plugins: [ApiSnapshot()],
 })
